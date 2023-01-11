3c4af5a56f654c953267ae71c4a86fe3d638faa1
PunnettSong <punnettsong@gmail.com>
2023-01-11 10:39:12 +0700

    add assessment data

---
+++ main.py
@@ -0,0 +1,15 @@
+
+"""_summary_
+    Adding For Loop & While Loop Example
+"""
+def main():
+    ebooks = ["python", "perl", "ruby"]
+    for item in ebooks:
+        if item == "perl":
+            print(item)
+            break
+        else:
+            print(item)
+            break
+if __name__ == "__main__":
+    main()
