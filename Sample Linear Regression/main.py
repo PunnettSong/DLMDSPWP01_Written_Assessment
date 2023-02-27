5f63dd83ca4fa4a706bdec87d04d30b0b72744e4
PunnettSong <punnettsong@gmail.com>
2023-02-27 15:59:46 +0700

    Add function into the applications

--- Sample Linear Regression/main.py
+++ Sample Linear Regression/main.py
@@ -15,6 +15,7 @@ from sklearn.metrics import mean_squared_error, r2_score
 import joblib
 
 #Load training data
+"""
 training_list = ['Training_01.csv', 'Training_02.csv', 'Training_03.csv', 'Training_04.csv']
 
 df_0 = pd.read_csv(training_list[0], nrows=50).to_numpy()
@@ -25,9 +26,16 @@ df_3 = pd.read_csv(training_list[3], nrows=50).to_numpy()
 
 X_train_0 = np.array(df_0[:,0]).reshape(-1,df_0.shape[0])
 y_train_0 = np.array(df_0[:,1]).reshape(-1,df_0.shape[0])
+"""
 
+df_train = pd.read_csv("train.csv").to_numpy()
+df_test = pd.read_csv("test.csv").to_numpy()
+df_ideal = pd.read_csv('ideal.csv').to_numpy()
 
-#Set X & Y training values
+#Set X training values
+
+X_train = np.array(df_train[:,0]).reshape(-1,df_train.shape[0])
+
 
 """
 #Set X training values
@@ -42,7 +50,9 @@ y_train_1 = df_1.Weight
 y_train_2 = df_2.Weight
 y_train_3 = df_3.Weight
 """
-#Set X & Y test values
+
+#Set X & Y test 
+"""
 df_test = pd.read_csv('Test_01.csv').to_numpy()
 X_test = np.array(df_test[:,0]).reshape(-1,df_test.shape[0])
 y_test = np.array(df_test[:,1]).reshape(-1,df_test.shape[0])
@@ -51,7 +61,7 @@ y_test = np.array(df_test[:,1]).reshape(-1,df_test.shape[0])
 df_prod = pd.read_csv('Production-01.csv', nrows=50).to_numpy()
 X_prod = np.array(df_prod[:,0]).reshape(-1,df_prod.shape[0])
 y_prod = np.array(df_prod[:,1]).reshape(-1,df_prod.shape[0])
-
+"""
 
 """
 # Load the diabetes dataset
@@ -80,31 +90,39 @@ regr.fit(diabetes_X_train, diabetes_y_train)
 
 # Code here
 # Create linear regression object
-regr = linear_model.LinearRegression()
 
+regr = linear_model.LinearRegression()
 
 # Train the model using the training sets
 filename = 'finalized_model.sav'
 
+num_col = df_train.shape[1]
+
+y_train = np.array(df_train[:,0]).reshape(-1,df_train.shape[0])
+regr = joblib.load(filename)
+regr.fit(X_train, y_train)
+joblib.dump(regr, filename)
+plt.plot(X_train, y_train, color="blue", linewidth=3)
+plt.xticks(())
+plt.yticks(())
+
 """for i in training_list:
     df_0 = pd.read_csv(i, nrows=50).to_numpy()
-"""
-
-X_train_0 = np.array(df_0[:,0]).reshape(-1,df_0.shape[0])
-y_train_0 = np.array(df_0[:,1]).reshape(-1,df_0.shape[0])
-regr = joblib.load(filename)
-regr.fit(X_train_0, y_train_0)
-joblib.dump(regr, filename)
+"""     
 
 """
 # Make predictions using the testing set
 diabetes_y_pred = regr.predict(diabetes_X_test)
 """
-
+"""
+X_test = np.array(df_test[:,0]).reshape(-1,df_test.shape[0])
 # Make prediction using the testing set
+
 print('X_test:')
 print(X_test)
+
 y_pred = regr.predict(X_test)
+"""
 
 print('y_pred:')
 
@@ -117,7 +135,7 @@ print("Coefficients: \n", regr.coef_)
 # The mean squared error
 print("Mean squared error: %.2f" % mean_squared_error(diabetes_y_test, diabetes_y_pred))
 # The coefficient of determination: 1 is perfect prediction
-print("Coefficient of determination: %.2f" % r2_score(diabetes_y_test, diabetes_y_pred))
+print("Coefficient of determination: %.2f" % r2_score(diabetes_y_test, diabaetes_y_pred))
 
 # Plot outputs
 plt.scatter(diabetes_X_test, diabetes_y_test, color="black")
@@ -129,7 +147,7 @@ plt.yticks(())
 plt.show()
 """
 
-
+"""
 #The coefficients
 print("Coefficients: \n", regr.coef_)
 #The mean squared error
@@ -146,4 +164,5 @@ plt.xticks(())
 plt.yticks(())
 
 plt.show()
+"""
 
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 """
 DLMDSPWP01_Written Assessment
 
@@ -12,6 +14,7 @@ Given:
     .     .
     Xn    Yn
     
+Uses training data to CHOOSE the four ideal functions which are the best fit out of the fifty provided
 
 """
 
@@ -28,51 +31,119 @@ Evaluate: Determine how accurate the model predictions are
 
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
 from matplotlib import pyplot as plt
-from sklearn.datasets import load_iris
+
+train_file = 'train.csv'
+test_file = 'test.csv'
+ideal_file = 'ideal.csv'
+
+df = pd.read_csv(train_file)
+
+#Set X & Y values
+x = df.x
+y = [df.y1, df.y2, df.y3, df.y4]
+n = df.shape[0]
+
+#Save the slope, y-intercept, y-deivation squared
+
+slope_list = []
+
+y_intercept_list = []
+
+ysd_list = []
+
+#My Line & My Model
+myline = np.linspace(-20, 22, 400)
+
+y_value = 0
+
+ 
+def least_Sq(x, y, n):
+    
+    global y_value
+    
+    # Sigma Variables
+    Sig_X = sum(x)
+    Sig_Y = sum(y)
+    Sig_XY = sum(x*y)
+    Sig_X_sq = sum(x*x)
+    
+    
+    #Formula of the Slope
+    m = (n*(Sig_XY) - (Sig_X * Sig_Y)) / ((n*Sig_X_sq) - (Sig_X * Sig_X))
+    
+    #Save slope
+    slope_list.append(m)
+    
+    print("The Slope is: " + str(m))
+    
+    # Formula of the constant (Y-intercept)
+    b = (Sig_Y - (m*Sig_X)) / n
+    
+    # Save y-intercept
+    
+    y_intercept_list.append(b)
+    
+    print("The Y-intercept is: " + str(b))
+    
+    #Least Squared Formula
+    
+    y_value = m*x + b
+    
+def plot(x, y):
+    # Plot for checking
+    plt.plot(x, y, label="Data", linewidth=2, color='red')
+    
+    mymodel = np.poly1d(np.polyfit(x, y_value, 4))
+    
+    plt.plot(myline, mymodel(myline), label="Least Squared Line", linewidth=2, color='blue')
+
+    plt.legend()
+    plt.grid(True, color="k")
+    plt.ylabel('x')
+    plt.xlabel('y')
+    plt.show()
+    
+
+def ysd(y, n):
+    
+    # Find the mean of y
+    
+    y_mean = sum(y) / n
+    
+    #Subtract each value with mean
+    
+    y_sub = y - y_mean
+    
+    #Squared the new value
+    
+    y_sq = y_sub * y_sub
+    
+    #Sum the squared
+    
+    ysd = sum(y_sq)
+    
+    ysd_list.append(ysd)
+    
 
 def Train_Data():
     
-    training_list = ['Training_01.csv', 'Training_02.csv', 'Training_03.csv', 'Training_04.csv']
-    for i in training_list:
-        df = pd.read_csv(i)
+    #Find the least squared function for each the training data function (y)
+    for i in y:
+        #Calculate Least Squared Equation
+        least_Sq(x, i, n)
         
-        #Set X & Y values
-        x = df.Height
-        y = df.Weight
+        print(y_value)
         
+        # Test Ploting Data
+        plot(x, i)
         
-        #Set least squared variables
-        Sig_X = sum(x)
-        Sig_Y = sum(y)
-        Sig_XY = sum(x*y)
-        Sig_X_sq = sum(x*x)
-        n = df.shape[0]
+        # Sum of y-squared(varience) deviation
+        ysd(i,n)
         
-        #Formula of the Slope
-        m = (n*(Sig_XY) - (Sig_X * Sig_Y)) / ((n*Sig_X_sq) - (Sig_X * Sig_X))
-        print("The Slope is: " + str(m))
-        
-        # Formula of the constant (Y-intercept)
-        b = (Sig_Y - (m*Sig_X)) / n
-        print("The Y-intercept is: " + str(b))
-        
-        #
-        y_value = m*x + b
-        
-        print(x, y_value)
-        
-        
-        
-        #Plot data
-        plt.scatter(x, y, label="Training_01")
-        plt.plot(x, y_value, label="Line Plot", linewidth=2)
-        plt.legend()
-        plt.grid(True, color="k")
-        plt.ylabel('Height')
-        plt.xlabel('Weight')
-        plt.show()
+        print(ysd_list)
+
+
     
 if __name__ == '__main__':
     Train_Data()
